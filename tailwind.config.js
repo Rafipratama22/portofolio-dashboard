f7f9502a9176d27f3df2a1e23e3aecb2589f73de
PT. Bank Negara Indonesia <bniw12@PTs-MacBook-Pro.local>
2022-01-28 23:50:52 +0700

    adding font family

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [],
+  theme: {
+    extend: {},
+    fontFamily: {
+      kaushan: ["Kaushan Script"],
+    },
+  },
+  plugins: [],
+  darkMode: false,
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
+};
